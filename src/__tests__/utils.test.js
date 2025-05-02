bba621a4908d39f93561d15ac4b009feca7aa6d1
shirocodes <ishirapt@gmail.com>
2025-05-02 10:31:45 +0300

    Completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,10 +2,26 @@ import { pointsForWord } from "../utils";
 
 describe("pointsForWord", () => {
   it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
-    const word = "test";
+    const word = "tEst";
 
     const points = pointsForWord(word);
 
     expect(points).toBe(7);
   });
+
+  it("returns 0 for empty string", () => {
+    expect(pointsForWord("")).toBe(0)
+  })
+
+  it("ignores numbers, symbols, and emojis", () => {
+    expect(pointsForWord("Test!🐶")).toBe(7)
+  })
+
+  it("throws an error for non-string inputs (number)", () => {
+    expect(() => pointsForWord(123)).toThrow(TypeError)
+  })
+
+  it("throws an error for non-string input (null)", () => {
+    expect(() => pointsForWord(null)).toThrow(TypeError);
+  })
 });
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,24 @@
+//checking for vowels and constants
+//checking for uppercase and lowercase
+//ignoring numbers, punctuations
+//show error for non-strings and empty strings
 export function pointsForWord(word) {
-    let points = 0
-    for (const char of word) {
-        if(["a", "e", "i", "o", "u"].includes(char)) {
-            points += 1
-        } else {
-            points +=2
-        }
+    if(typeof word !== "string") {
+        throw new TypeError("input must be a string")
     }
-    return points
+
+    if(word.length === 0) {
+        return 0
+    }
+    let points = 0 
+    for (const char of word) {
+        if(/[a-z]/i.test(char)) {
+            points += /[aeiou]/.test(char.toLowerCase()) ? 1 : 2
+        }
+        //else ignore numbers, punctuations and emojis
+    }
+    return points;
 }
+
+
+
